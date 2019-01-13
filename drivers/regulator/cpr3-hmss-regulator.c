5005751c5978f293428313f784db467b924d1768
Gustavo <38637402+characteress@users.noreply.github.com>
2019-01-13 00:13:08 -0200

    Undervolt regulator

--- drivers/regulator/cpr3-hmss-regulator.c
+++ drivers/regulator/cpr3-hmss-regulator.c
@@ -366,19 +366,19 @@ enum msm8996_cpr_partial_binning {
 /* Open loop voltage fuse reference voltages in microvolts for MSM8996 v1/v2 */
 static const int msm8996_v1_v2_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	705000, /* Place holder entry for LowSVS */
-	705000,
-	865000,
-	1015000,
+	685000, /* Place holder entry for LowSVS */
+	685000,
+	845000,
+	985000,
 };
 
 /* Open loop voltage fuse reference voltages in microvolts for MSM8996 v3 */
 static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	705000, /* Place holder entry for LowSVS */
-	705000,
-	865000,
-	1110000,
+	685000, /* Place holder entry for LowSVS */
+	685000,
+	845000,
+	1080000,
 };
 
 /*
@@ -388,10 +388,10 @@ static const int msm8996_v3_hmss_fuse_ref_volt[MSM8996_HMSS_FUSE_CORNERS] = {
 static const int msm8996_v3_speed_bin1_rev5_hmss_fuse_ref_volt[
 						MSM8996_HMSS_FUSE_CORNERS] = {
 	605000,
-	705000, /* Place holder entry for LowSVS */
-	705000,
-	865000,
-	1040000,
+	685000, /* Place holder entry for LowSVS */
+	685000,
+	845000,
+	1010000,
 };
 
 /* Defines mapping from retention fuse values to voltages in microvolts */
